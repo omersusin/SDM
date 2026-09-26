499503c28f427966d641007183b7aa3850737cae
omersusin <86467497+omersusin@users.noreply.github.com>
2026-09-26 04:46:41 +0300

    Restore Lifecycle import

--- android/app/src/main/kotlin/grab/bit/android/util/compose/ObserveUiVisibility.kt
+++ android/app/src/main/kotlin/grab/bit/android/util/compose/ObserveUiVisibility.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.currentStateAsState
 
